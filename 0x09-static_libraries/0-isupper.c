e544f1bf8f55eee8eed7d662602e581917e5dd5b
brianchemase <brianchemo@gmail.com>
2023-01-13 11:17:41 -0800

    files

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,11 +1,15 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
+
+/**
+ * main - check code
+ * A program in C that checks for uppercase letters
+ * Return: 1 or 0
+ */
 
 int _isupper(int c)
 {
-    return (c >= 'A' && c <= 'Z');
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
-
