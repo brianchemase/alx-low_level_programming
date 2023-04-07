bee33430665e1a6c4c7e9be5da648fb57085e3ce
brianchemase <brianchemo@gmail.com>
2023-04-07 12:50:26 +0300

    update

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+
+typedef struct {
+    /* define the structure of a hash table entry */
+    /* e.g. a key-value pair and a pointer to the next entry */
+} hash_entry_t;
+
+typedef struct {
+    unsigned long int size; /* size of the array */
+    hash_entry_t **buckets; /* array of pointers to hash entries */
+} hash_table_t;
+
+/* hash function that maps a string key to an index in the array */
+unsigned long int hash_function(const char *key, unsigned long int size)
+{
+    /* implement a suitable hash function */
+    /* e.g. the djb2 algorithm by Dan Bernstein */
+}
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *table = (hash_table_t *) malloc(sizeof(hash_table_t));
+    if (table == NULL) {
+        return NULL; /* out of memory */
+    }
+    table->size = size;
+    table->buckets = (hash_entry_t **) calloc(size, sizeof(hash_entry_t *));
+    if (table->buckets == NULL) {
+        free(table);
+        return NULL; /* out of memory */
+    }
+    return table;
+}
