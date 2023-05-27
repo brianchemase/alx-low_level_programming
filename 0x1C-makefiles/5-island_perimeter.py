7f4ffb79141fc08cd6800cef788a100987c4d991
brianchemase <brianchemo@gmail.com>
2023-05-27 19:19:35 +0300

    upload

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,41 +1,33 @@
 #!/usr/bin/python3
+"""
+module for perimeter of island
+"""
+
 
 def island_perimeter(grid):
-    """
-    Calculates the perimeter of the island described in the grid.
-
+    """returns the perimeter of the island
     Args:
-        grid (list of list of integers): The grid representing the island.
-
-    Returns:
-        int: The perimeter of the island.
-
-    Raises:
-        ValueError: If the grid is empty or not rectangular.
-
+         grid (list) = list of integers
     """
 
-    if not grid or not grid[0]:
-        raise ValueError("Invalid grid: Grid is empty.")
-    
-    rows = len(grid)
-    cols = len(grid[0])
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
 
-    perimeter = 0
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                # checking for boundary or a lake in horizontal and vertical directions
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+                if (j < width - 1 and grid[i][j + 1] == 1):
+                    edges += 1
+                if (i < height - 1 and grid[i + 1][j] == 1):
+                    edges += 1
 
-    for row in range(rows):
-        for col in range(cols):
-            if grid[row][col] == 1:  # Found a land cell
-                perimeter += 4  # Add 4 to the perimeter for each land cell
+    return size * 4 - edges
 
-                # Check the adjacent cells and subtract 1 for each adjacent land cell
-                if row > 0 and grid[row - 1][col] == 1:
-                    perimeter -= 1
-                if row < rows - 1 and grid[row + 1][col] == 1:
-                    perimeter -= 1
-                if col > 0 and grid[row][col - 1] == 1:
-                    perimeter -= 1
-                if col < cols - 1 and grid[row][col + 1] == 1:
-                    perimeter -= 1
-
-    return perimeter
