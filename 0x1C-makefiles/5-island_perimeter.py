68cdc638513f720a56c9d1129564a4942acfd22f
brianchemase <brianchemo@gmail.com>
2023-05-27 19:15:03 +0300

    upload task on python

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,27 +1,41 @@
 #!/usr/bin/python3
-"""
-module for perimeter of island
-"""
-
 
 def island_perimeter(grid):
-    """returns the perimeter of the island
+    """
+    Calculates the perimeter of the island described in the grid.
+
     Args:
-         grid (list) = list of integers
+        grid (list of list of integers): The grid representing the island.
+
+    Returns:
+        int: The perimeter of the island.
+
+    Raises:
+        ValueError: If the grid is empty or not rectangular.
+
     """
 
-    width = len(grid[0])
-    height = len(grid)
-    edges = 0
-    size = 0
+    if not grid or not grid[0]:
+        raise ValueError("Invalid grid: Grid is empty.")
+    
+    rows = len(grid)
+    cols = len(grid[0])
 
-    for i in range(height):
-        for j in range(width):
-            if grid[i][j] == 1:
-                size += 1
-                # checking for boundery or a lake
-                if (j > 0 and grid[i][j - 1] == 1):
-                    edges += 1
-                if (i > 0 and grid[i - 1][j] == 1):
-                    edges += 1
-    return size * 4 - edges * 2
+    perimeter = 0
+
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:  # Found a land cell
+                perimeter += 4  # Add 4 to the perimeter for each land cell
+
+                # Check the adjacent cells and subtract 1 for each adjacent land cell
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 1
+                if row < rows - 1 and grid[row + 1][col] == 1:
+                    perimeter -= 1
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 1
+                if col < cols - 1 and grid[row][col + 1] == 1:
+                    perimeter -= 1
+
+    return perimeter
